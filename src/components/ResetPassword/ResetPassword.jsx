5fdaf9dbfb8ac93fca55b045a4487e9f2756cf95
f-rahim <freelancerrahim26@gmail.com>
2023-05-16 22:03:41 +0600

    Added password reset functionality

--- src/components/ResetPassword/ResetPassword.jsx
+++ src/components/ResetPassword/ResetPassword.jsx
@@ -1,18 +1,41 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
+import { sendPasswordResetEmail } from "firebase/auth";
+import auth from "../../firebase/firebase";
+import { useState } from "react";
 
 function ResetPassword() {
+  const [resetMailStatus, setResetMailStatus] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleResetPassword = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+
+    setResetMailStatus();
+    setError();
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        setResetMailStatus(
+          "Reset mail send. Plase check you mail and reset password."
+        );
+      })
+      .catch((error) => {
+        setError(error.code);
+      });
+  };
+
   return (
     <div className="login-register-container reset-password-component">
-      <Form>
+      <Form onSubmit={handleResetPassword}>
         <h3 className="text-center mb-3">Reset Password</h3>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We will never share your email with anyone else.
-          </Form.Text>
+          <Form.Control type="email" placeholder="Enter email" name="email" />
+          <Form.Text className="text-success">{resetMailStatus}</Form.Text>
+          <br />
+          <Form.Text className="text-danger">{error}</Form.Text>
         </Form.Group>
 
         <div className="text-center mb-5">
@@ -21,6 +44,7 @@ function ResetPassword() {
           </Button>
         </div>
       </Form>
+
       <p className="m-0 mt-2 small">
         Dont have a account? Please <Link to="/register">Register Now</Link>
       </p>
